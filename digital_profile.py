158561c71aeb52f240d6b763f4a5cf8cd029b93e
413-10-chanapa <54291@yupparaj.ac.th>
2026-06-25 09:56:37 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("My Digital Profile")
+
+
+st.header("แนะนำตัว")
+st.write("นางสาวชนาภา ไชยวรรณ")
+st.write("ชื่อเล่น เมษา")
+st.write("เลขที่ 10 ม.4/13")
+
+
+st.header("เป็าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย :**อยากถ่ายทอดความรู้ให้ผู้อื่น ")
+st.write("สิ่งที่อยากเป็นในอนาคต:ครู (teacher) ")
